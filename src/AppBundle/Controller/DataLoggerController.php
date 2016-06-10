f4cceaafed1782443416ac3085e4a81db92f6d7c
Mariano Heller <marianoheller91@gmail.com>
2016-06-10 16:33:10 -0300

    Added json parsing and device validation.

--- src/AppBundle/Controller/DataLoggerController.php
+++ src/AppBundle/Controller/DataLoggerController.php
@@ -12,6 +12,7 @@ use AppBundle\Entity\Datalog;
 
 
 const numeroDeCanales = 32;
+const passwordDevice = "INTI1957";
 
 class DataLoggerController extends Controller
 {
@@ -24,20 +25,44 @@ class DataLoggerController extends Controller
             $ret = new Response("Ajax request");
         }
         else {
-            $sql = "select * from (
-                        select * from (
-                            select * from datalog order by hora desc
-                            )horaOrdered order by fecha desc
-                        ) fechaOrdered
-                    group by sensor_id";
-            $dql = "SELECT d FROM (
-                      SELECT y FROM (
-                        SELECT x FROM AppBundle:datalog x ORDER BY x.hora
-                      )y  ORDER BY y.fecha DESC
-                    ) d GROUP BY d.sensor_id";
-            $dqlSimpificada = "SELECT d FROM AppBundle:Datalog d";
+            $sql = "SELECT  a.*
+                    FROM    datalog a
+                            INNER JOIN
+                            (
+                                SELECT sensor_id, MAX(hora) max_date
+                                FROM    datalog
+                                GROUP BY sensor_id
+                            ) b ON a.sensor_id = b.sensor_id AND
+                    a.hora = b.max_date
+                    ORDER BY sensor_id ASC";
+
+
+            $dql = "SELECT a, b
+                    FROM AppBundle:Datalog a
+                    JOIN
+                    (
+                        SELECT b.sensorId, MAX(hora) max_date
+                        FROM AppBundle:Datalog b
+                        GROUP BY b.sensorId
+                    ) b ON a.sensorId = b=sensorId AND
+                    a.hora = b.max_date
+                    ORDER BY d.sensorId ASC";
+
+            $dqlSimpificada = "SELECT d FROM AppBundle:Datalog d ORDER BY d.hora DESC";
 
             $em = $this->getDoctrine()->getManager();
+
+/*            $subquery = $em->createQueryBuilder()
+                ->select('b.sensorId', 'MAX(b.hora) as max_date')
+                ->from('AppBundle:Datalog', 'b')
+                ->groupBy('b.sensorId')
+                ->getDQL();
+            $qb = $em->createQueryBuilder();
+            $qb->select('a')
+                ->from('AppBundle:Datalog', 'a')
+                ->where($qb->expr()->notIn('a.id', $subquery));*/
+
+
             $query = $em->createQuery($dqlSimpificada);
             $datalogArray = $query->getResult();
 
@@ -83,29 +108,68 @@ class DataLoggerController extends Controller
         );
 
         $content = $request->getContent();
-        if (!empty($content)) {
-            $paramsArray = json_decode($content, true);
-            $params = $content;
+        $contentType = $request->getContentType();
 
+        if ( $contentType != "json" || empty($content) ) {          //CONTENIDO NOT JSON
+            $params = "Params Empty or not json. Content type: ".$contentType;
+            $response->setContent($params);
+
+        }
+        else {
+            $em = $this->getDoctrine()->getManager();
+            $data = json_decode($content);
+
+            foreach ($data as $name => $value) {
+                if ( $name == "LOGIN") {
+                    foreach ($value as $entry) {
+                        $password = $entry->password;
+                    }
+                }
+            }
+            if ($password != passwordDevice) {          //PASSWORD ERRONEO
+                $params = "Params Empty or not json. Content type: ".$contentType;
+                $response->setContent($params);
+            }
+            else {                                        //ELSE -> PARSEO LA DATA
+                $contItemsAdded = 0;
+                foreach ($data as $name => $value) {
+                    if ( $name == "CANALES") {
+                        foreach ($value as $entry) {
+                            $datalog = new Datalog();
+                            $datalog->setSensorId(intval($entry->canal));
+                            $datalog->setMedicion($entry->temperatura);
+                            $newDate = date("m-d-Y", strtotime($entry->fecha));
+                            $fecha=new \DateTime($newDate);
+                            $datalog->setFecha($fecha);
+                            $hora=new \DateTime($entry->hora);
+                            $datalog->setHora($hora);
+
+                            $contItemsAdded++;
+                            $em->persist($datalog);
+                            $em->flush();
+                        }
+                    }
+                }
+                $stringResponse = 'Recibidas '.$contItemsAdded.' mediciones nuevas<br/>';
+                $response->setContent($stringResponse);
+            }
+
+            //TODO Mejor respuesta al recibir datos. Por ej una tabla con la data recibida.
+
+/*
             $datalog = new Datalog();
-            $datalog->setSensorId(intval($paramsArray['sensor_id']));
-            $datalog->setMedicion($paramsArray['medicion']);
+            $datalog->setSensorId(intval($paramsArray['canal']));
+            $datalog->setMedicion($paramsArray['temperatura']);
             $newDate = date("m-d-Y", strtotime($paramsArray['fecha']));
             $fecha=new \DateTime($newDate);
             $datalog->setFecha($fecha);
             $hora=new \DateTime($paramsArray['hora']);
             $datalog->setHora($hora);
 
-            $em = $this->getDoctrine()->getManager();
             $em->persist($datalog);
             $em->flush();
-            $response->setContent('Saved new product with id '.$datalog->getId());
+            $response->setContent('Saved new data with id '.$datalog->getId()); */
         }
-        else {
-            $params = "Params Empty";
-            $response->setContent($params);
-        }
-
 
         return $response;
     }
